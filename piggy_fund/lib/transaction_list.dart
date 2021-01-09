6da48131beca44c896b0e9d0d351316680b33268
Vivian Wu <wu.vivian00@gmail.com>
2021-01-09 03:21:06 -0800

    added widget to list transactions

---
+++ piggy_fund/lib/transaction_list.dart
@@ -0,0 +1,32 @@
+import 'package:flutter/material.dart';
+import 'package:piggy_fund/model/transaction.dart';
+
+class TransactionList extends StatelessWidget {
+  final List<Transaction> items;
+  TransactionList({Key key, this.items});
+
+  @override
+  Widget build(BuildContext context) {
+    return ListView.builder(
+      physics: NeverScrollableScrollPhysics(),
+      shrinkWrap: true,
+      itemCount: items.length,
+      itemBuilder: (context, index) {
+        return Container(
+          padding: EdgeInsets.all(2),
+          height: 140,
+          child: Card(
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: <Widget>[
+                Text("${items[index].transactionAmt.toString()}"),
+                Text("${items[index].type.toString()}"),
+                // Text("${items[index].date.toString()}"),
+              ],
+            ),
+          ),
+        );
+      },
+    );
+  }
+}
